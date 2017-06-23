e1cf0118e458d3e067d084536d393a680fbced20
blakkisrud <blakkisrud@gmail.com>
2017-06-23 16:03:24 +0200

    Full hook on clean

--- clean.sh
+++ clean.sh
@@ -1,5 +1,9 @@
 mv *.aux aux_files
 mv *.log aux_files
 mv main.pdf output.pdf
-git rm main.pdf
 git add output.pdf
+git add aux_files/*.aux
+git add aux_files/*.log
+git add *.enl
+git add *.bib
+git add *.Data
